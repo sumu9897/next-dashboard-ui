1d51f970537b2ed61ec244eeb389922a2025f9c0
sumu9897 <mohammad.sumon987@gmail.com>
2025-01-26 23:34:52 +0600

    Navbar Design Done

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navbar } from "./(components)/Navbar";
+
+export const DashboardWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className={`light flex bg-gray-50 text-gray-950 w-full min-h-screen`}>
+      Sidebar
+      <main className="flex flex-col w-full py-7 px-9 bg-gray-50 md:pl-24">
+        <Navbar/>
+        {children}</main>
+    </div>
+  );
+};
